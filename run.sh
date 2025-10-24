342a04c04f54bd1ca33ff6d191dcdaf6944e1538
yuezhouhu <yuezhouhu@outlook.com>
2025-10-23 20:25:41 -0700

    Add code

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the lists
+version="gsm8k-target-pythia-1.4b-ref-pythia-31m-3epoch"
+model="EleutherAI/pythia-31m"
+
+remove_before_slash() {
+    local input_string="$1"
+    local output_string="${input_string##*/}"
+    echo "$output_string"
+}
+
+sh run_train.sh "$version" "$model"
---
+++ run_train.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Check if the required arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <version> <model>"
+    exit 1
+fi
+
+# Assign the arguments to variables
+version="$1" # change this to your desired version name
+model="$2" # this can be either "gpt2*" or "opt-*", or leave it blank, which means using the default model.
+
+# Check if the ./logs folder exists
+if [ ! -d "./logs" ]; then
+    # If the folder doesn't exist, create it
+    mkdir "./logs"
+    echo "Created ./logs folder"
+else
+    echo "./logs folder already exists"
+fi
+
+# Check if the ./logs/$version folder exists
+if [ ! -d "./logs/$version" ]; then
+    # If the folder doesn't exist, create it
+    mkdir "./logs/$version"
+    echo "Created ./logs/$version folder"
+else
+    echo "./logs/$version folder already exists"
+fi
+
+nvidia-smi
+
+accelerate launch --config_file accelerate_configs/zero1.yaml train.py \
+    --draft_model_name_or_path $model \
+    --target_model_name_or_path "path/to/target/checkpoint-1404" \
+    \
+    --data_name gsm8k \
+    \
+    --bf16 True \
+    --output_dir "./checkpoints/$version" \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 1 \
+    --per_device_eval_batch_size 8 \
+    --eval_accumulation_steps 1 \
+    --eval_strategy "epoch" \
+    --save_strategy "epoch" \
+    --batch_eval_metrics True \
+    --prediction_loss_only True \
+    --eval_on_start \
+    --save_only_model True \
+    --learning_rate 3e-4 \
+    --weight_decay 0.1 \
+    --lr_scheduler_type "constant" \
+    --logging_steps 10 \
+    --report_to tensorboard \
+    --logging_dir "./logs/$version" \
+    --gradient_checkpointing True
